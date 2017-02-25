d0d524ef8411fadedbfe737cb7b8377805d1ac69
Wolfvak <soherrera1@hotmail.com>
2017-02-25 11:27:59 -0300

    Old Spider framebuffer fix (untested) (#1)

--- source/start.s
+++ source/start.s
@@ -3,22 +3,22 @@
 .align 4
 .arm
 
-@ if the binary is booted from Brahma/CakeHax/k9lh
+@ if the binary is booted from Brahma/CakeHax/k9lh or 2xrsa
 @ the entrypoint is <start + 0x0>
-@ framebuffers are already set
+@ framebuffers are already set unless 2xrsa
 _start:
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
+    mov r0, #0x20000000
+    ldr r0, [r0]
+    ldr r1, .2xrsa_magic @ "FUCK"
+    cmp r0, r1
+    beq _set_2xrsa_framebuffers
+    b _skip_gw
     nop
     nop
     nop
     nop
     nop @ dummy
-    b _skip_gw
+    .2xrsa_magic: .word 0x4b435546
 
 @ if the binary is booted from the GW exploit
 @ the entrypoint is <start + 0x30>
@@ -65,15 +65,30 @@ _start_gw:
     stmia r0, {r1,r2,r3}
     @ framebuffers properly set
 
+    bl ctr_cache_clean_and_flush_all
+
     ldr r3, .entry
     bx r3
 
-.gw_fba:  .word 0x080FFFC0
-.gw_fbb:  .word 0x080FFFD0
-.cakehax: .word 0x23FFFE00
-.entry:   .word 0x23F00000
+.gw_fba:    .word 0x080FFFC0 @ gw top framebuffer magic
+.gw_fbb:    .word 0x080FFFD0 @ ^ bottom
+
+.2xrsa_fba: .word 0x181E6000 @ 2xrsa top framebuffer start
+.2xrsa_fbb: .word 0x1848F000 @ ^ bottom
+
+.cakehax:   .word 0x23FFFE00 @ cakehax fb struct location
+.entry:     .word 0x23F00000 @ brahma execution base address
+
+_set_2xrsa_framebuffers:
+    ldr r0, .cakehax
+    ldr r1, .2xrsa_fba
+    mov r2, r1
+    ldr r3, .2xrsa_fbb
+    stmia r0, {r1,r2,r3}
+    @ fall through
 
 _skip_gw:
+    bl ctr_cache_clean_and_flush_all
     @ Disable caches / mpu
     mrc p15, 0, r4, c1, c0, 0  @ read control register
     bic r4, #(1<<12)           @ - instruction cache disable
@@ -83,9 +98,8 @@ _skip_gw:
 
     @ Clear bss
     ldr r0, =__bss_start
-    ldr r1, =__end__
+    ldr r1, =__bss_end
     mov r2, #0
-
     .bss_clr:
         cmp r0, r1
         strlt r2, [r0], #4
@@ -146,5 +160,35 @@ _skip_gw:
 
     blx main
     b _start
+
+@ code from https://github.com/gemarcano/libctr9/blob/6a3c0748c3e508e50eedcfa6f639bb29823eda4e/src/ctr_cache.s#L38
+ctr_cache_clean_and_flush_all:
+    mov r1, #0 @segment
+
+    1:  @outer_loop
+        mov r0, #0 @line
+        2:  @inner_loop
+            orr r2, r1, r0  @make r2 be the resulting combination of
+                                  @segment and line
+
+            mcr p15, 0, r2, c7, c14, 2  @Clean and flush the line
+
+            add r0, r0, #0x20  @move line to next line
+
+            @The number of lines depends on the cache size, 0x400 for the
+            @3DS data cache since it has 8KB of cache. Refer to the ARM
+            @documentation for more details.
+            cmp r0, #0x400
+            bne 2b  @inner_loop
+
+        add r1, r1, #0x40000000
+        cmp r1, #0x0
+        bne 1b  @outer_loop
+
+    mov r0, #0
+    mcr p15, 0, r0, c7, c5, 0  @Flush instruction cache
+    mcr p15, 0, r0, c7, c10, 4  @drain write buffer
+    bx lr
+
 
 .pool
